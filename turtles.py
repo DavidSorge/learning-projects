bf1f777d5644b762d9ca1d19ba76fc1604d94f7a
DavidSorge <david.c.sorge@gmail.com>
2019-04-05 12:22:58 -0400

    Updating turtles.py

--- turtles.py
+++ turtles.py
@@ -12,36 +12,54 @@
 import turtle
 
 wn = turtle.Screen()
-wn. bgcolor("seagreen")
+wn. bgcolor("turquoise")
 wn.title("Freshie and Freddy")
 
 Freshie = turtle.Turtle()   # Create Freshie
 Freshie.color('green')
+Freshie.shape("turtle")
 Freshie.pensize(5)
+Freshie.speed(5)
 
 Freddy = turtle.Turtle()    # Create Freddy
 Freddy.color('orange red')
 Freddy.pensize(3)
+Freddy.shape("triangle")
+Freshie.speed(10)
 
-Freshie.forward(80)         # Draw equilateral triangle with Freshie
-Freshie.left(120)
-Freshie.forward(80)
-Freshie.left(120)
-Freshie.forward(80)
-Freshie.left(120)           # Complete Triangle
+for a in range(3):
+    Freshie.forward(80)         # Draw equilateral triangle with Freshie
+    Freshie.left(120)
 
 Freshie.right(180)          # Turn Freshie around
 Freshie.forward(80)          # Move Freshie away from origin
 
-Freddy.forward(50)          # Draw square with Freddy
-Freddy.left(90)
-Freddy.forward(50)
-Freddy.left(90)
-Freddy.forward(50)
-Freddy.left(90)
-Freddy.forward(50)
-Freddy.left(90)
+#Assign a list to a variable
+clrs = ["yellow", "red", "orange", "orange red"]
+for c in clrs:
+    Freddy.color(c)
+    Freddy.forward(50)          # Draw square with Freddy
+    Freddy.left(90)
+
+Freddy.penup()
+Freddy.forward(100) #moves Freddy without drawing a line
+Freddy.pendown()
+
+
+
+
+Stamper = turtle.Turtle()
+Stamper.shape("turtle")
+Stamper.color("blue")
+
+Stamper.penup()
+size = 20
+for j in range(30):
+    Stamper.stamp()  #Leave an impression on the canvas
+    size = size + 3  #increase size with each iteration
+    Stamper.forward(size)   # moves Stamper
+    Stamper.right(24)       # and turns.
+
+Stamper.color("red")
 
 wn.mainloop()
-
-
